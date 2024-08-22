69659fa7da1f7cd34d8d65395da65c39652c201e
wchgatti <163424962+wchgatti@users.noreply.github.com>
2024-08-22 09:51:34 -0300

    form

---
+++ Formulários/IMC.js
@@ -0,0 +1,12 @@
+  // ENTRADA
+  let peso = Number(prompt("Seu peso:"))
+  let altura = Number(prompt("Sua altura:"))
+
+  // PROCESSAMENTO
+  let imc = peso / (altura * altura)
+
+  // SAÍDA
+//   console.log("IMC: ", imc)
+
+//   SAÍDA
+  document.write("IMC: ",imc)
